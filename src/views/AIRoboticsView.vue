d6f531acfbb1b2d3281eea4139fb4ca22b02e10a
km222uq <km222uq@student.lnu.se>
2023-09-07 18:07:52 +0200

    added video for AI Academia

--- src/views/AIRoboticsView.vue
+++ src/views/AIRoboticsView.vue
@@ -5,10 +5,19 @@
       You can pose questions to Misty in various languages and have the robot
       respond as if it were a person or character. Misty will be used by both
       students and our researchers in contexts concerned with understanding and
-      developing children’s learning about AI and computational thinking.
+      developing children's learning about AI and computational thinking.
     </p>
     <div class="media">
       <img src="../assets/mistyshot.jpeg" alt="Misty shot" class="media-img" />
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/PjLTeFrWI7Q?si=z4k13pIvCgaG3YRz"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen
+      ></iframe>
     </div>
   </div>
   <FooterComp />
